b37334ddd9840111fa9d619dca7690978ae0264b
Mahesh Kulkarni <kulkarni.mak172@gmail.com>
2016-06-26 15:12:49 +0200

    Added some more samples

--- 06-materialStarterKit/app.js
+++ 06-materialStarterKit/app.js
@@ -36,10 +36,4 @@ angular
             email: 'ThomasBurleson@Gmail.com',
             phone: ''
         };
-    })
-
-
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Use of this source code is governed by an MIT-style license that can be in foundin the LICENSE file at http://material.angularjs.org/license.
- */
+    });
